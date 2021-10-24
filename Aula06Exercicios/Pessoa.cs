b0c84f9d4572aa99888caeca35c78af49b20a847
Majulia <majulialcarvalho@gmail.com>
2021-10-24 17:50:35 -0300

    aula 06

---
+++ Aula06Exercicios/Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+ class Pessoa
+{
+    public double peso, altura;
+    
+    public double imc()
+    {
+        return peso / (altura * altura);
+    }
+
+    public string situacao(double imc)
+    {
+        string retorno;
+
+        if (imc < 18.5)
+        {
+            retorno = "Abaixo do Peso";
+        }else if (imc < 25)
+        {
+            retorno = "Peso normal";
+        }else if (imc < 30)
+        {
+            retorno = "Acima do peso";
+        }else if (imc < 35)
+        {
+            retorno = "Obesidade I";
+        }else if (imc < 40)
+        {
+            retorno = "Obesidade 2";
+        }
+        else
+        {
+            retorno = "Obesidade Mórbida";
+        }
+
+        return retorno;
+        
+    }
+
+    public void mensagem()
+    {
+        double obterPeso = imc();
+        string obterSituacao = situacao(obterPeso);
+
+        Console.WriteLine($"Seu peso IMC é de {obterPeso} e sua situação é {obterSituacao}");
+        
+    }
+}
---
+++ Aula06Exercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Aula06Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa um = new Pessoa();
+            um.peso = 80;
+            um.altura = 1.80;
+            um.mensagem();
+        }
+    }
+}
